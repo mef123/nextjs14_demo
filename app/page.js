d4447886b3c23dc062babede035db7fe372f85c1
MefchU <hristo.netov@gmail.com>
2026-01-13 02:07:44 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn now maina
+          Learn now
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
@@ -78,7 +78,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          New Examples 
+          Examples 
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
