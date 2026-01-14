24a78998da8577d288c0062c07b4b6dac066cd66
MefchU <hristo.netov@gmail.com>
2026-01-14 02:56:42 +0200

    Update page.js

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read our docs
+            Read docs
           </a>
         </div>
       </main>
