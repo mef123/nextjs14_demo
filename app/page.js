f5f45e9fe8d624bedf87515f0320febb7634cf97
MefchU <hristo.netov@gmail.com>
2026-02-19 22:06:33 +0200

    Update link text in page.js

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read docs 1234! 
+            Read docs
           </a>
         </div>
       </main>
