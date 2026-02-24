d02943e633138c3dc88baa465f83f3ea95ff7581
MefchU <hristo.netov@gmail.com>
2026-02-25 00:16:01 +0200

    Remove extra text from links in page.js

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read docs 1234!
+            Read docs
           </a>
         </div>
       </main>
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn !
+          Learn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
