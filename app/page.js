30702100d0f856bea500f3edc98e4eed3f539a1d
MefchU <hristo.netov@gmail.com>
2026-02-28 18:46:40 +0200

    Update footer link text to include '12'

--- app/page.js
+++ app/page.js
@@ -45,7 +45,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Read docs
+            Read docs 12
           </a>
         </div>
       </main>
